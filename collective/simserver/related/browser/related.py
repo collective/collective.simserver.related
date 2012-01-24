f7e6ef819ff2b089f44995ae444be78774c961bd
Christian Ledermann <christian.ledermann@gmail.com>
2012-01-24 16:41:57 +0300

    fixes for new version of restsims

--- collective/simserver/related/browser/related.py
+++ collective/simserver/related/browser/related.py
@@ -34,15 +34,10 @@ class RelatedItemsView(BrowserView):
     def getsimilaritems(self):
         """ query simserver for related items, exclude self.context"""
         contextuid = self.context.UID()
-        service = coreutils.get_session_server()
-        try:
-            simserveritems = service.find_similar(contextuid)
-        except ValueError:
-            simserveritems = []
-            logger.info('Document not indexed')
-        suids =[s[0] for s in simserveritems
+        service = coreutils.SimService()
+        simserveritems = service.query([contextuid])
+        suids =[s[0] for s in simserveritems[contextuid]
                     if contextuid != s[0]]
-
         brains = self.portal_catalog(UID = suids)
         items = {}
         for brain in brains:
@@ -51,7 +46,7 @@ class RelatedItemsView(BrowserView):
                     'title': brain.Title,
                     'desc': brain.Description}
         results = []
-        for item in simserveritems:
+        for item in simserveritems[contextuid]:
             if item[0] in items:
                 result = {}
                 result = items[item[0]]
